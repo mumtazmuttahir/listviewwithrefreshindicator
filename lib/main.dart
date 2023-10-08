b2de8657b0977f7d383090c08ac785d6064f9902
Muttahir Mumtaz <muttahir.mumtaz@hotmail.com>
2023-10-08 02:56:24 +0200

    inifinte scrolling and refresh

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -7,39 +10,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'List View Demo with refresh indicator'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +32,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final scrollController = ScrollController();
+  List<String> items = [];
+  int page = 1;
+  bool hasMore = true;
+  bool isLoading = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    fetchDataFromTheServer();
+
+    scrollController.addListener(() {
+      //To make sure the user is scrolling till the bottom of the screen
+      if (scrollController.position.maxScrollExtent ==
+          scrollController.offset) {
+        fetchDataFromTheServer();
+      }
     });
+  }
+
+  Future fetchDataFromTheServer() async {
+    if (isLoading) return;
+    isLoading = true;
+    const limit = 25;
+    final url = Uri.parse(
+        'https://jsonplaceholder.typicode.com/posts?_limit=$limit&_page=$page');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List newItems = json.decode(response.body);
+      setState(() {
+        page++;
+        isLoading = false;
+        if (newItems.length < limit) {
+          hasMore = false;
+        }
+        items.addAll(newItems.map<String>((item) {
+          final id = item['id'];
+          return 'Item $id';
+        }).toList());
+      });
+    }
+  }
+
+  Future refresh() async {
+    setState(() {
+      page = 0;
+      hasMore = true;
+      isLoading = false;
+      items.clear();
+    });
+
+    fetchDataFromTheServer();
+  }
+
+  //make sure to dispose the controller as it is a statful widget
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: RefreshIndicator(
+        onRefresh: refresh,
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8.0),
+          //The scroll controller must be add here
+          //Otherwise the scrolling will not update
+          controller: scrollController,
+          //Add 1 here so that the last element in the list view
+          //is either a progress indicator or end of list text
+          itemCount: items.length + 1,
+          itemBuilder: (context, index) {
+            if (index < items.length) {
+              final item = items[index];
+              return ListTile(
+                title: Text(item),
+              );
+            } else {
+              return Padding(
+                padding: const EdgeInsets.all(32.0),
+                child: Center(
+                  child: hasMore == true
+                      ? const CircularProgressIndicator()
+                      : const Text('End of list'),
+                ),
+              );
+            }
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
